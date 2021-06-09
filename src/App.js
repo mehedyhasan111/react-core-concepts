b3066fcbb28943a7c71d28397099e20cc683d015
Mehedy Hasan <hasanmehedy111@gmail.com>
2021-06-10 00:25:48 +0600

    core

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['shakib khan' , 'arfin shuvo','kaji maruf','jontu da']
+  const products = [
+    {name:'Photoshop',price:'$99.90'},
+    {name:'Illustrator',price:'$100.00'},
+    {name:'PDF Reader',price:'$10.00'},
+    {name:'premiure el',price:'$234.00'}
+];
+
+ 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit done <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>My first react  paragraph </p>
+        <Counter></Counter>
+        
+        <ul>
+          {
+            nayoks.map(nayok=> <li>{nayok}</li>)
+          }
+          {
+            products.map(pd=><li>{pd.name}</li>)
+          }
+        </ul>
+
+
+
+
+
+        {/* <Person name={nayoks[0]} nayika="shabana"> </Person>
+        <Person name="Bappraj" nayika="none"> </Person>
+
+        <Person name="rubel" nayika="moushumi"> </Person>
+        <Person name="salman shah" nayika="shabnur"> </Person> */}
+
+
+        {/* <Product name={products[0].name} price={products[0].price}></Product>
+        <Product name={products[1].name} price={products[1].price}></Product>
+        <Product name={products[2].name} price={products[2].price}></Product> */}
+
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product> */}
+
+        {
+          products.map(pd=><Product product={pd}></Product>)
+
+        }
+
+
+
+        
       </header>
     </div>
   );
 }
 
+function Counter(){
+  const [count,setCounter] = useState(11);
+  return(
+    <div>
+      <h3>Count:{count}</h3>
+    </div>
+  )
+}
+
+function Product(props){
+  const productStyle = {
+    border:'2px solid gray',
+    borderRadius:'5px',
+    backgroundColor:'lightgrey',
+    height:'200px',
+    width:'200px',
+    color:'black',
+    float:'left'
+
+  }
+const {name,price}=props.product;
+  return(
+    <div style={productStyle}>
+      {/* first way */}
+      {/* <h2>{props.name}</h2>
+      <h3>{props.price}</h3>
+      <button>Buy Now</button> */}
+
+      {/* second way */}
+      {/* <h2>{props.product.name}</h2>
+      <h3>{props.product.price}</h3>
+      <button>Buy Now</button> */}
+
+      {/* third way */}
+      <h2>{name}</h2>
+      <h3>{price}</h3>
+      <button>Buy Now</button>
+      
+    </div>
+  );
+}
+
+// function Person(props){
+//   const style = {
+//     border : '2px solid red',
+//     // backgroundColor : 'white',
+//     color : 'cyan',
+//     margin : '10px'
+//   }
+//   return (
+//     <div style={style}>
+//       <h1>{props.name}</h1>
+//       <p>Hero of : {props.nayika}</p>
+//     </div>
+ 
+//   );
+
+// }
 export default App;
